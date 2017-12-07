7379159a710043bc3410d83c53043489b85a7d32
AnandSundar <anandsundar96@gmail.com>
2017-12-07 15:47:48 -0500

    added navigation links and implemeted functionality to store workouts in webSQL

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -1,6 +1,6 @@
 // Initialize app
 var myApp = new Framework7();
-
+var db;
 
 // If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
@@ -14,30 +14,72 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+    //use open databse (webSql -  built in db in web browser)
+    //1000000 bytes = 1MB (db size)
+    db = window.openDatabase('workouttracker', '1.0', 'Workout Tracker', 1000000);
+    //function call to create db if not exists
+    createDatabase();
+    //get the pre existing workouts from db
+
 });
-
-
-// Now we need to run the code that will be executed only for About page.
-
-// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
-myApp.onPageInit('about', function (page) {
-    // Do something here for "about" page
-
-})
-
-// Option 2. Using one 'pageInit' event handler for all pages:
-$$(document).on('pageInit', function (e) {
-    // Get page data from event data
-    var page = e.detail.page;
-
-    if (page.name === 'about') {
-        // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
+myApp.onPageInit('add', function (page) {
+  //catch the submit event
+  $$('#workout-form').on('submit', function (e) {
+    var data = {
+      id: guid(),
+      title: $$('#title').val(),
+      date: $$('#date').val(),
+      type: $$('#type').val(),
+      length: $$('#length').val()
     }
+    addWorkout(data);
+  });
+
 })
 
-// Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
-    // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
-})
+function createDatabase() {
+  db.transaction(createTable,
+    //error function
+    //tx = transaction
+    function(tx, err) {
+      alert ('DB error ' + err);
+    },
+    //succes
+    function () {
+      console.log('Database and table created...');
+  });
+}
+
+
+function createTable(tx) {
+  //drop table syntax
+  //tx.executeSql('DROP TABLE IF EXISTS workouts');
+   tx.executeSql('CREATE TABLE IF NOT EXISTS workouts (id unique, title, date, type, length)');
+}
+
+//code snippet taken from stackoverflow
+function guid() {
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
+}
+
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+}
+
+//store workouts in database
+function addWorkout(workout){
+  db.transaction(function (tx) {
+    tx.executeSql('INSERT INTO workouts (id, title, date, type, length)  VALUES("'+workout.id+'","'+workout.title+'","'+workout.date+'","'+workout.type+'","'+workout.length+'")');
+  },
+  //error
+  function (tx, err) {
+    console.log(err + ' ' + tx.message);
+  },
+  //success
+  function () {
+      window.location.href = 'index.html';
+  });
+}
